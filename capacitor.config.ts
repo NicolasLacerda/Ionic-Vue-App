b7f9f17b7317e3c7678f507a443f56a15c981be4
NicolasLacerda <nicolas.lacerda.work@gmail.com>
2024-12-03 14:52:44 -0300

    03/12/2024
    App Doens´t  open bug fixed.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'brazwrap',
-  webDir: 'dist'
+  appId: "brazwrap.app",
+  appName: "brazwrap",
+  webDir: "dist",
 };
 
 export default config;
